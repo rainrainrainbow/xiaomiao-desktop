e3e68c6243a26db830549cc0d96412feb8331f72
rainrainrainbow <1448282415@qq.com>
2026-07-23 17:38:25 +0800

    Add missing ST7735 register definitions

--- main/main.c
+++ main/main.c
@@ -70,6 +70,16 @@ static const char *TAG = "MAIN";
 #define ST7735_RAMWR    0x2C
 #define ST7735_MADCTL   0x36
 #define ST7735_COLMOD   0x3A
+#define ST7735_FRMCTR1  0xB1
+#define ST7735_FRMCTR2  0xB2
+#define ST7735_FRMCTR3  0xB3
+#define ST7735_INVCTR   0xB4
+#define ST7735_PWCTR1   0xC0
+#define ST7735_PWCTR2   0xC1
+#define ST7735_PWCTR3   0xC2
+#define ST7735_PWCTR4   0xC3
+#define ST7735_PWCTR5   0xC4
+#define ST7735_VMCTR1   0xC5
 #define ST7735_GMCTRP1  0xE0
 #define ST7735_GMCTRN1  0xE1
 
