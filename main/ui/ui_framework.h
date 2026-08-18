5155baff608fe5cdc9f48c866b698f845dbae686
rainrainrainbow <1448282415@qq.com>
2026-08-18 21:37:07 +0800

    v68: bump version to v68, build 2026-08-24

--- main/ui/ui_framework.h
+++ main/ui/ui_framework.h
@@ -26,8 +26,8 @@
 #define DOCK_H              8    // 底部导航栏高度
 
 /* ========== 系统版本信息 ========== */
-#define XIAOMIAO_VERSION    "v67"
-#define XIAOMIAO_BUILD      "2026-08-23"
+#define XIAOMIAO_VERSION    "v68"
+#define XIAOMIAO_BUILD      "2026-08-24"
 
 /* ========== 页面类型 ========== */
 typedef enum {
